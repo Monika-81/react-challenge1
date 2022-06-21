e7a9d1b8343fae1b5d623e0427d94b08025e6d3b
Monika Jakobsson <nimrin81@gmail.com>
2022-06-21 01:10:51 +0000

    Handling Events Challenge

---
+++ my-app/src/components/NavBarForm.js
@@ -0,0 +1,54 @@
+import React, { Component } from "react";
+import css from "./css/NavBarForm.module.css"
+
+class NavBarForm extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            buttonText: "Login",
+            isLoggedIn: false
+        };
+    }
+
+    handleClick() {
+        this.setState((prevState, prevProps) => {
+            console.log('prev state', prevState)
+            return {
+                isLoggedIn: this.state.isLoggedIn === "false" ? "true" : "false",
+                buttonText: this.state.buttonText === "log out" ? "log in" : "log out",
+            }
+        })
+    }
+
+    render() {
+        return (
+        <div className={css.NavBar}>
+            <h1>My Gallery</h1>
+            {
+                this.state.isLoggedIn ? (
+                    <div>
+                        <form>
+                            <label>Username:</label>
+                            <input placeholder="username"></input>
+                            <label>Password:</label>
+                            <input placeholder="password"></input>                                
+                            <button onClick={() => this.handleClick()}>Submit</button>
+                        </form>
+                    </div>
+                ) : (
+                    <div>
+                        <span>{this.state.message}</span>
+                        <button onClick={() => this.handleClick()}>
+                            {this.state.buttonText}
+                        </button>
+                    </div>
+                )
+            }
+        </div>
+        )
+    };
+}
+
+export default NavBarForm;
+
